2c8c2cd071490f7ed2fbd4e1df1944c3315b8e49
Jakub Matyjewicz <niewiem826@gmail.com>
2020-05-03 01:36:09 +0100

    1

---
+++ src/main/java/jmatyjewiczle/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,20 @@
+package jmatyjewiczle.coronavirustracker.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.StringReader;
+@Getter
+@Setter
+@ToString
+public class LocationStats {
+
+    private String state;
+    private String country;
+    private int latestTotalCases;
+    private int diffFromPrevDay;
+
+
+
+}
